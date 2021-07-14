de757029ad543064e04e0d7d69b0b209087ad151
Emi Bekirova <85579116+embekirova@users.noreply.github.com>
2021-07-14 21:32:18 +0300

    Home page content editing

---
+++ Web/LeadersCorner.Web.ViewModels/Home/IndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace LeadersCorner.Web.ViewModels.Home
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+   public class IndexViewModel
+    {
+        public int ArticlesCout { get; set; }
+
+        public int CourseCount { get; set; }
+    }
+}
--- Web/LeadersCorner.Web/Controllers/HomeController.cs
+++ Web/LeadersCorner.Web/Controllers/HomeController.cs
@@ -1,16 +1,35 @@
 ﻿namespace LeadersCorner.Web.Controllers
 {
     using System.Diagnostics;
-
+    using System.Linq;
+    using LeadersCorner.Data.Common.Repositories;
+    using LeadersCorner.Data.Models;
     using LeadersCorner.Web.ViewModels;
-
+    using LeadersCorner.Web.ViewModels.Home;
     using Microsoft.AspNetCore.Mvc;
 
     public class HomeController : BaseController
     {
+        private readonly IDeletableEntityRepository<Article> articleRespository;
+        private readonly IDeletableEntityRepository<Course> courseRepository;
+
+        public HomeController(
+            IDeletableEntityRepository<Article> articleRespository,
+            IDeletableEntityRepository<Course> courseRepository)
+        {
+            this.articleRespository = articleRespository;
+            this.courseRepository = courseRepository;
+        }
+
         public IActionResult Index()
         {
-            return this.View();
+            var viewModel = new IndexViewModel
+            {
+                ArticlesCout = this.articleRespository.All().Count(),
+                CourseCount = this.courseRepository.All().Count(),
+            };
+
+            return this.View(viewModel);
         }
 
         public IActionResult Privacy()
